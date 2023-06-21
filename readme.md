fef6e1c916716db45c3da2b0a497b3b8e106aa4f
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-06-21 14:39:36 +0900

    update:都道府県のAPIデータを取得
    都道府県のAPIをfetchを利用してデータ取得した

--- readme.md
+++ readme.md
@@ -4,3 +4,5 @@
 ・GitHub 運用には GitHubflow を利用して作業を進める
 
 ・ESlint の設定 - 学習する際に参考にしたサイト　https://www.wakuwakubank.com/posts/716-javascript-eslint-prettier/
+
+・都道府県の API を取得する
--- src/api/todouhuken.tsx
+++ src/api/todouhuken.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function Todouhuken() {
-  const [state, setState] = useState();
+  const [state, setState] = useState([]);
   const url = " https://opendata.resas-portal.go.jp/api/v1/prefectures";
   useEffect(() => {
     fetch(url, {
@@ -20,6 +20,22 @@ export default function Todouhuken() {
     <>
       <div className="japan">
         <h2>都道府県</h2>
+        <form>
+          {state === undefined
+            ? "Loading..."
+            : state.map((doc: any) => (
+                <div key={doc.prefCode}>
+                  <input
+                    type="checkbox"
+                    name={doc.prefName}
+                    value={doc.prefCode}
+                    onClick={() => console.log(doc.prefCode)}
+                  />
+                  <label>{doc.prefName}</label>
+                </div>
+              ))}
+          <button type="submit">比較する</button>
+        </form>
       </div>
     </>
   );
