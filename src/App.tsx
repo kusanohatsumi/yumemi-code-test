31639eb502ed3531d8982e0af5c72f386e902176
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-07-01 09:40:03 +0900

    update:データの取得場所を変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
 import "./App.css";
-import Checkbox from "./api/checkbox";
+
+import { useEffect, useState } from "react";
+import Graph from "./api/graph";
 
 function App() {
+  const [state, setState] = useState([]);
+  const [fetchData, setFetchData] = useState("");
+
+  // APIから都道府県の一覧データを取得
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL, {
+      headers: {
+        "X-API-KEY": import.meta.env.VITE_API_KEY,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setState(data.result);
+      })
+      .catch(() => alert("error"));
+  }, []);
+
   return (
     <>
       <h1>Title</h1>
-      <Checkbox />
+
+      <div className="checkbox">
+        <h2>都道府県</h2>
+        <form>
+          {state === undefined
+            ? "Loading..."
+            : state.map((doc: any) => (
+                <div className="formItem" key={doc.prefCode}>
+                  <input
+                    type="checkbox"
+                    id={doc.prefCode}
+                    name={doc.prefName}
+                    value={doc.prefCode}
+                    onChange={() => {
+                      // 人口構成のデータをコンソールに表示
+                      setFetchData(
+                        import.meta.env.VITE_API_URL_POPULATION + doc.prefCode
+                      );
+                    }}
+                  />
+                  <label htmlFor={doc.prefCode}>{doc.prefName}</label>
+                </div>
+              ))}
+        </form>
+        <Graph props={fetchData} />
+      </div>
     </>
   );
 }
--- src/api/graph.tsx
+++ src/api/graph.tsx
@@ -1,7 +1,14 @@
-export default function Graph() {
-  return (
-    <>
-      <h2>人口のグラフ描画</h2>
-    </>
-  );
+import { set } from "firebase/database";
+import { useEffect, useState } from "react";
+
+export default function Graph(props: any) {
+  const [population, setPopulation] = useState([]);
+  console.log(props);
+  // if (props === "") {
+  //   console.log("空です");
+  // } else {
+  //   console.log("空ではありません");
+  // }
+
+  return <>{/* グラフ描画領域 */}</>;
 }
