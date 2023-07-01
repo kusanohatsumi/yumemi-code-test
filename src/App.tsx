dff235027dea193b9ebdb7d4da2767e15af4816e
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-07-01 15:38:10 +0900

    update:App.tsxでfetch処理に変更
    Propsでデータを受け渡したかったので、App.tsxで処理を行うように変更した

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,59 @@
 import "./App.css";
 import Checkbox from "./api/checkbox";
 
+import { useEffect, useState } from "react";
+import { Population, Prefectures } from "./types/type";
+
 function App() {
+  const [prefecuture, setPrefecture] = useState<Prefectures[]>([]);
+  const [spot_prefecuture, setSpot_Prefectures] = useState<Prefectures[]>([]);
+
+  const [population, setPopulation] = useState<Population[]>([]);
+
+  // 都道府県のAPIを取得する
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL_PREFECUTURE, {
+      headers: {
+        "X-API-KEY": import.meta.env.VITE_API_KEY,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPrefecture(data.result);
+      })
+      .catch(() => alert("error"));
+  }, []);
+
+  // 選択した都道府県のIDをstateに格納する
+  const handleAddPrefecture = (value: any) => {
+    // チェックがついたときの処理
+    if (value.target.checked === true) {
+      setSpot_Prefectures([...prefecuture, value.target.value]);
+      // console.log(new Set([...prefecuture, value.target.value]));
+
+      // 人口のAPIを取得する
+      fetch(`${import.meta.env.VITE_API_URL_POPULATION}=${value.target.id}`, {
+        headers: {
+          "X-API-KEY": import.meta.env.VITE_API_KEY,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPopulation(data.result);
+          console.log(data.result);
+        })
+        .catch(() => alert("error"));
+    }
+    // チェックが外れたときの処理
+    else {
+      prefecuture.splice(prefecuture.indexOf(value.target.value), 1);
+    }
+  };
+
   return (
     <>
       <h1>Title</h1>
-      <Checkbox />
+      <Checkbox prefData={prefecuture} onChange={handleAddPrefecture} />
     </>
   );
 }
--- src/api/checkbox.tsx
+++ src/api/checkbox.tsx
@@ -1,67 +1,21 @@
-import { useEffect, useState } from "react";
-import { Population, Prefectures } from "../types/type";
-
-export default function Checkbox() {
-  const [state, setState] = useState([]);
-  const [prefecuture, setPrefecture] = useState<Prefectures[]>([]);
-  const [population, setPopulation] = useState<Population[]>([]);
-
-  // 都道府県のAPIを取得する
-  useEffect(() => {
-    fetch(import.meta.env.VITE_API_URL_PREFECUTURE, {
-      headers: {
-        "X-API-KEY": import.meta.env.VITE_API_KEY,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setState(data.result);
-      })
-      .catch(() => alert("error"));
-  }, []);
-
-  // 選択した都道府県のIDをstateに格納する
-  const handleAddPrefecture = (value: any) => {
-    // チェックがついたときの処理
-    if (value.target.checked === true) {
-      setPrefecture([...prefecuture, value.target.value]);
-      // console.log(new Set([...prefecuture, value.target.value]));
-
-      // 人口のAPIを取得する
-      fetch(`${import.meta.env.VITE_API_URL_POPULATION}=${value.target.id}`, {
-        headers: {
-          "X-API-KEY": import.meta.env.VITE_API_KEY,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setPopulation(data.result);
-          console.log(data.result);
-        })
-        .catch(() => alert("error"));
-    }
-    // チェックが外れたときの処理
-    else {
-      prefecuture.splice(prefecuture.indexOf(value.target.value), 1);
-      console.log(prefecuture);
-    }
-  };
+export default function Checkbox(props: any) {
+  const prefData = props.prefData;
 
   return (
     <>
       <div className="checkbox">
         <h2>都道府県</h2>
         <form>
-          {state === undefined
+          {prefData === undefined
             ? "Loading..."
-            : state.map((doc: any) => (
+            : prefData.map((doc: any) => (
                 <div className="formItem" key={doc.prefCode}>
                   <input
                     type="checkbox"
                     id={doc.prefCode}
                     name={doc.prefName}
                     value={doc.prefCode}
-                    onClick={handleAddPrefecture}
+                    onChange={props.onChange}
                   />
                   <label htmlFor={doc.prefCode}>{doc.prefName}</label>
                 </div>
