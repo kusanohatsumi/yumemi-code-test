f51d197ea638fc4eb633274425eeecf40b497db2
HatsumiKusano <kusano.hatsumi@gmail.com>
2023-07-03 01:21:16 +0900

    update:1つ前の状態を修正前に復元させた

--- src/api/checkbox.tsx
+++ src/api/checkbox.tsx
@@ -1,21 +1,24 @@
 export default function Checkbox(props: any) {
   const prefData = props.prefData;
+  console.log(prefData);
 
   return (
     <>
       <div className="checkbox">
-        {prefData.map((doc: any) => (
-          <div className="checkboxItem" key={doc.prefCode}>
-            <input
-              type="checkbox"
-              id={doc.prefCode}
-              name={doc.prefName}
-              value={doc.prefCode}
-              onChange={props.onChange}
-            />
-            <label htmlFor={doc.prefCode}>{doc.prefName}</label>
-          </div>
-        ))}
+        {prefData === undefined
+          ? console.log("Loading...")
+          : prefData.map((doc: any) => (
+              <div className="checkboxItem" key={doc.prefCode}>
+                <input
+                  type="checkbox"
+                  id={doc.prefCode}
+                  name={doc.prefName}
+                  value={doc.prefCode}
+                  onChange={props.onChange}
+                />
+                <label htmlFor={doc.prefCode}>{doc.prefName}</label>
+              </div>
+            ))}
       </div>
     </>
   );
